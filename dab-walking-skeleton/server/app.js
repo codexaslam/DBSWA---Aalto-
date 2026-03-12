1d704eef0811506f36b7e2321211e1e1749486c3
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2026-03-12 22:55:30 +0200

    Message passing with Redis

--- dab-walking-skeleton/server/app.js
+++ dab-walking-skeleton/server/app.js
@@ -1,12 +1,35 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
+import { Redis } from "ioredis";
+
+// Do not modify these
+const REDIS_CONTAINER_NAME = Deno.env.get("REDIS_HOST");
+const REDIS_CONTAINER_PORT = Deno.env.get("REDIS_PORT");
+const QUEUE_NAME = "PING_QUEUE";
+
+const redis = new Redis(
+  Number.parseInt(REDIS_CONTAINER_PORT),
+  REDIS_CONTAINER_NAME,
+);
 
 const app = new Hono();
 
-app.use("/*", cors());
+// Add the expected routes here
+app.post("/ping", async (c) => {
+  await redis.lpush(QUEUE_NAME, "ping");
+  return c.text("pong");
+});
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello world!" });
+app.get("/pong", async (c) => {
+  const count = await redis.llen(QUEUE_NAME);
+  if (count === 0) {
+    return c.text("Queue empty");
+  }
+  const message = await redis.rpop(QUEUE_NAME);
+  // Handle theoretical race where queue became empty between checks
+  if (!message) {
+    return c.text("Queue empty");
+  }
+  return c.text(message);
 });
 
 export default app;
